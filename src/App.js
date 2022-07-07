5486b19775c8b60c3a70bf86eb83778c40a33371
heohyoyeong <heohyoyeong@gmail.com>
2022-07-07 23:09:58 +0900

    add travis yaml file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn change success
+          Learn change successs
         </a>
       </header>
     </div>
